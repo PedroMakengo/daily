c90d6e304581a07e691a67980af35f49e10fd66c
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-11 14:37:28 -0300

    Adicionando novos componentes

---
+++ assets/js/daily.js
@@ -0,0 +1,18 @@
+$(".owl-carousel").owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 20,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
