a8c8fa3e1ec61381d47e86e2139e950f69d33f6a
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-11 15:02:05 -0300

    Terminado o projeto

--- assets/js/daily.js
+++ assets/js/daily.js
@@ -1,18 +1,23 @@
-$(".owl-carousel").owlCarousel({
-  rtl: true,
-  loop: true,
-  margin: 20,
-  nav: false,
-  autoplay: true,
-  responsive: {
-    0: {
-      items: 1,
+$(function () {
+  AOS.init();
+
+  $(".owl-carousel").owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 20,
+    nav: false,
+    autoplay: true,
+    smartSpeed: 2e3,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
     },
-    600: {
-      items: 1,
-    },
-    1000: {
-      items: 1,
-    },
-  },
+  });
 });
